356d9857b361e11bc52df2ea40ae205ccd0553f0
Hussain Wali <hussainwali743@gmail.com>
2025-02-04 17:30:58 +0100

    v 0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import { Link, Routes, Route } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUtensils } from '@fortawesome/free-solid-svg-icons';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Home from './pages/Home/Home';
-import Menu from './pages/Menu/Menu';
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
-import Footer from './components/Footer/Footer';
+// import { Link, Routes, Route } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Home from './pages/Home/Home';
+// import Menu from './pages/Menu/Menu';
+// import About from './pages/About/About';
+// import Contact from './pages/Contact/Contact';
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
